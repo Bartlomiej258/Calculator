5b3c6e0ac3531bdabd9f970b1187c6f740af02be
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2022-06-26 23:24:39 +0200

    create calculator

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.calculator;
+
+public class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int substraction(int a, int b) {
+        return a - b;
+    }
+}
